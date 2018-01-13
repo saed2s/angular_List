ba629f2032931559ffea423ec63661d500ea9b8a
Basel Akasha <baselakasha@gmail.com>
2018-01-13 11:15:08 +0000

    [Add files #28] add librarys local files

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ function download(filename, text) {
     var pom = document.createElement('a');
     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     pom.setAttribute('download', filename);
-
     if (document.createEvent) {
         var event = document.createEvent('MouseEvents');
         event.initEvent('click', true, true);
