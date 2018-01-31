b57f89dd6cabcf3bb93575cdccce029969dfcc07
baselakasha <baselakasha@gmail.com>
2018-01-31 19:37:11 +0000

    [Update #35] add partials and add sittings partial and add download name

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,16 @@
-
+var myApp = angular.module("app",["ngRoute"]);
+myApp.config(function($routeProvider){
+        $routeProvider
+        .when("/",{
+            templateUrl:"src/partial/main.html",
+            controller:"mainController"
+        })
+        .when("/setting",{
+            templateUrl:"src/partial/setting.html",
+            controller:"settingController"
+	     })
+	     .otherwise({redirectTo:'/'});
+});
 function download(filename, text) {
 	
 	 "use strict";
@@ -37,96 +49,77 @@ function toUppSort(array){
     "use strict";
      var finalArray= upperArray(array);
      return finalArray.sort() ;
+} 
+var  itemsJs =  ["test 1","Test 2","Test 3"]; /// default items 
+itemsJs = toUppSort(itemsJs);  /// sort and make each element of array capital    
+if(store){ ///  if storeJs loaded
+    var array =store.get("data");  /// get stored data
+	 if(array){ /// if stored data is not empty
+        itemsJs = array;
+    }
 }
-    
-window.onload=function(){
-    "use strict"; 
-     var  itemsJs =  ["test 1","Test 2","Test 3"]; /// default items 
-     itemsJs = toUppSort(itemsJs);  /// sort and make each element of array capital    
-  	  if(store){ ///  if storeJs loaded
-         var array =store.get("data");  /// get stored data
-	      if(array){ /// if stored data is not empty
-	          itemsJs = array;
-		   }
-	  }
-	  else{
-         alert("Proplem when load store.js library please try reload the page or another browser ");
-  	  }
-	 
-    function searchInItems(txt){  /// search in objects has class (temsP)
-        "use strict";
-        var obj = document.getElementsByClassName("itemsP"),
-            value = new RegExp(txt.trim(),"i");
-        for (var i = 0; i<obj.length; i++){
-            var ma =obj[i].innerText.trim().match(value);
-            if (ma != null){
-                if(ma == obj[i].innerText.trim()){
-                    return obj[i];
-                }
-            }
-        }
-    }
-    function light(obj){  
-	    /// scroll to the element and make background green for 1.1 sec
-       "use strict";
-       obj.scrollIntoView(); 
-       obj.style.backgroundColor ="#97ff80";
-       setTimeout(function(){
-	        /// return background to white
-           obj.style.backgroundColor="white";
-       },1100);
-    }
-	 function arrayUpt(es){ /// es must to be $scope
-		 /// update items array and store it 
-        "use strict";
-        itemsJs=toUppSort(itemsJs); /// make first letter for each element of array capital and sort 
-        es.items=itemsJs; /// update items 
-        if(store){ 
-            store.set("data",itemsJs); /// store items
-        }
-       
-    }
-    ///hide loading 
-    var loadDiv = document.getElementById("loadDiv");
+else{
+    alert("Proplem when load store.js library please try reload the page or another browser ");
+} 
+function searchInItems(txt){  /// search in objects has class (temsP)
+    "use strict";
+     var obj = document.getElementsByClassName("itemsP"),
+          value = new RegExp(txt.trim(),"i");
+     for (var i = 0; i<obj.length; i++){
+         var ma =obj[i].innerText.trim().match(value);
+         if (ma != null){
+             if(ma == obj[i].innerText.trim()){
+                 return obj[i];
+              }
+         }
+      }
+}
+ function light(obj){  
+	  /// scroll to the element and make background green for 1.1 sec
+    "use strict";
+    obj.scrollIntoView(); 
+    obj.style.backgroundColor ="#97ff80";
     setTimeout(function(){
-	     $("#loadDiv").fadeOut(200);
-    },500);
-
-    /// nice scroll 
-    $("#mainDiv").niceScroll({
-	       cursorcolor:"#DD0031",
-          cursorwidth:"5px"
-    });
-
-    var myApp = angular.module("app",[]);
-    myApp.controller("con",function($scope){
+	      /// return background to white
+        obj.style.backgroundColor="white";
+    },1100);
+}
+function arrayUpt(es){ /// es must to be $scope
+    /// update items array and store it 
+    "use strict";
+     itemsJs=toUppSort(itemsJs); /// make first letter for each element of array capital and sort 
+     es.items=itemsJs; /// update items 
+     if(store){ 
+         store.set("data",itemsJs); /// store items
+     }
+}
+myApp.controller("mainController",function($scope){
+    "use strict";
+     $scope.items = toUppSort(itemsJs); 
+     $scope.edit = function(itemTxt){ /// Edit function
+         var obj = searchInItems(itemTxt), /// found item user need to edit by item text (come from index.html )
+              arrayInd= itemsJs.indexOf(itemTxt), /// found itemsJs array index 
+              userEnter = prompt("Edit Value",itemTxt);
+         if(userEnter == "" || userEnter== null){} /// if user left prompt empty or click cancel
+         else{ 
+             itemsJs[arrayInd] = userEnter;  /// replace old value to new value
+             arrayUpt($scope); 
+             setTimeout(function(){
+                 var newObj = searchInItems(userEnter);
+                 light(newObj);
+             },100);
+         }
+    };
+    $scope.del = function(txt){
         "use strict";
-        $scope.items = toUppSort(itemsJs); 
-        $scope.edit = function(itemTxt){ /// Edit function
-            var obj = searchInItems(itemTxt), /// found item user need to edit by item text (come from index.html )
-                arrayInd= itemsJs.indexOf(itemTxt), /// found itemsJs array index 
-                userEnter = prompt("Edit Value",id);
-            if(userEnter == "" || userEnter== null){} /// if user left prompt empty or click cancel
-            else{ 
-                itemsJs[arrayInd] = userEnter;  /// replace old value to new value
-                arrayUpt($scope); 
-                setTimeout(function(){
-                     var newObj = searchInItems(userEnter);
-                    light(newObj);
-                },100);
-            }
-        };
-        $scope.del = function(txt){
-            "use strict";
-            if(confirm(" Delete : (" + txt+ ") ?")){ /// get confirm from user , txt come from index.html 
-                var arrayIndexDel= itemsJs.indexOf(txt); /// found index of item in itemsJs array
-                itemsJs.splice(arrayIndexDel,1); /// delete the item from array
-                arrayUpt($scope);
-                
-            }
-        };
-        $scope.add = function(){
-          "use strict";
+         if(confirm(" Delete : (" + txt+ ") ?")){ /// get confirm from user , txt come from index.html 
+             var arrayIndexDel= itemsJs.indexOf(txt); /// found index of item in itemsJs array
+             itemsJs.splice(arrayIndexDel,1); /// delete the item from array
+             arrayUpt($scope);            
+         }
+     };
+     $scope.add = function(){
+         "use strict";
           var userAdd = prompt("Enter value :");
           if(userAdd=="" || userAdd == null){} /// if user left the prompt empty or click cancel
           else{
@@ -138,19 +131,92 @@ window.onload=function(){
               },100);
               
           }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-        };
-        $scope.export = function(){  
-         /// make a txt has items Each item in a line and download it 
-            var txt = "";
-            for(var i= 0;i<itemsJs.length;i++){
-                txt +=itemsJs[i] + "\n";
-    	      }
-	        download("list.txt",txt); 
-	     };
-    });
-    
+      };
+      $scope.export = function(){  
+      /// make a txt has items Each item in a line and download it 
+          
+          var txt = "";
+          for(var i= 0;i<itemsJs.length;i++){
+              txt +=itemsJs[i] + "\n";
+    	    }
+	      if(store.get("efn") == 0){
+	            var name = store.get("dfn");
+	      }
+	      else if(store.get("efn") ==1){
+		         var name = prompt("Enter download file name ");
+		    }
+		    else{
+			      store.set("efn",1);
+		         var name = prompt("Enter download file name ");
+			  }
+	       download(name,txt);
+	   };
+}); /// main controll close
+myApp.controller("settingController",function($scope){
+	 "use strict";
+	  var dfn = document.getElementById("dfnInp"),
+            txt =document.getElementById("txt");
+     if(store.get("efn")==0){
+        $scope.check0=true;
+        $scope.dfn=store.get("dfn");
+     }                 
+     else{
+         $scope.check1=true;        
+         dfn.disabled=true;
+      }        
+      $scope.efnChange=function(){        
+          if($scope.efn ==0){
+              dfn.disabled=false;
+              if($scope.dfn== undefined || $scope.dfn.length==0){
+                  dfn.style.borderColor="red";
+                  txt.style.borderColor="red";
+              }
+              store.set("efn",0);
+           }
+           else{
+                dfn.disabled=true;
+                dfn.style.borderColor="#c1c1c1";
+                txt.style.borderColor="#c1c1c1";
+                store.set("efn",1);
+            }
+            
+        
+    };  
+    (function eventLis(){
+        dfn.addEventListener("keyup",function(){
+            if($scope.dfn.length==0){
+                dfn.style.borderColor="red";
+                txt.style.borderColor="red";
+             }
+             else{
+                 dfn.style.borderColor="black";
+                 txt.style.borderColor="black";
+                 store.set("efn",0);
+                 store.set("dfn",$scope.dfn);
+            }              
+        });  
+      })();
+ });   
+
+window.onload=function(){
+    ///hide loading 
+    var loadDiv = document.getElementById("loadDiv"),
+          txt= document.getElementById("txt");
+    setTimeout(function(){
+        $("#loadDiv").fadeOut(200);
+    },500);
+  
+    /// nice scroll 
+   $("#mainDiv").niceScroll({
+       cursorcolor:"#DD0031",
+       cursorwidth:"5px"
+   });
+
+
+};
+     
         
     
 
-};
+
 
